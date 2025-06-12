a53f75d185a7babae304626857680ebd491181f7
Kakarla Meeraj <meeraj097@gmail.com>
2025-06-12 12:10:59 +0530

    Fix BlogViewSet definition and urls

--- backend/blogapp/urls.py
+++ backend/blogapp/urls.py
@@ -1,6 +1,5 @@
-from django.urls import path, include
-from rest_framework import routers
-from .views import BlogViewSet  # import your viewset
+from django.urls import path
+from .views import BlogListCreateView, BlogDetailView
 from django.contrib.auth.models import User
 from django.http import JsonResponse
 
@@ -10,10 +9,8 @@ def create_test_user(request):
         return JsonResponse({"message": "Test admin user created"})
     return JsonResponse({"message": "Admin already exists"})
 
-router = routers.DefaultRouter()
-router.register(r'blogs', BlogViewSet, basename='blog')
-
 urlpatterns = [
-    path('', include(router.urls)),  # registers /blogs/ endpoints automatically
-    path('create-admin/', create_test_user),  # keep this if you want the test user route
+    path('blogs/', BlogListCreateView.as_view(), name='blog-list-create'),
+    path('blogs/<int:pk>/', BlogDetailView.as_view(), name='blog-detail'),
+    path('create-admin/', create_test_user),
 ]
