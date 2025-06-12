2c29021b48b822bbb8799b29935bc8732f9e0702
Kakarla Meeraj <meeraj097@gmail.com>
2025-06-12 11:46:22 +0530

    Fix model references and add author username to BlogSerializer

--- backend/blog/views.py
+++ backend/blog/views.py
@@ -1,19 +1,15 @@
 from rest_framework import viewsets
 from rest_framework.permissions import BasePermission, SAFE_METHODS
-from .models import Blog
+from .models import BlogPost
 from .serializers import BlogSerializer
 
 class ReadOnlyOrAuthenticated(BasePermission):
-    """
-    Custom permission to allow read-only access for any user,
-    but write access only for authenticated users.
-    """
     def has_permission(self, request, view):
         if request.method in SAFE_METHODS:
             return True
         return request.user and request.user.is_authenticated
 
 class BlogViewSet(viewsets.ModelViewSet):
-    queryset = Blog.objects.all()
+    queryset = BlogPost.objects.all()
     serializer_class = BlogSerializer
     permission_classes = [ReadOnlyOrAuthenticated]
