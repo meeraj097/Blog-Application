b7ed88788a3734668163dba224ed51623674f003
Kakarla Meeraj <meeraj097@gmail.com>
2025-06-12 10:10:37 +0530

    Expose /create-admin/ endpoint at root level

--- backend/blogapi/urls.py
+++ backend/blogapi/urls.py
@@ -16,6 +16,7 @@ urlpatterns = [
     path('api/', include('blogapp.urls')),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('', include('blogapp.urls')), 
 ]
 from django.urls import path, include
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
