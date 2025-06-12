09dcdff74f64651cc71b54afeec63da87230ec63
Kakarla Meeraj <meeraj097@gmail.com>
2025-06-12 11:30:44 +0530

    Allow public read access to Blog API with ReadOnlyOrAuthenticated permission

--- backend/blog/views.py
+++ backend/blog/views.py
@@ -1,4 +1,19 @@
+from rest_framework import viewsets
+from rest_framework.permissions import BasePermission, SAFE_METHODS
+from .models import Blog
+from .serializers import BlogSerializer
+
+class ReadOnlyOrAuthenticated(BasePermission):
+    """
+    Custom permission to allow read-only access for any user,
+    but write access only for authenticated users.
+    """
+    def has_permission(self, request, view):
+        if request.method in SAFE_METHODS:
+            return True
+        return request.user and request.user.is_authenticated
+
 class BlogViewSet(viewsets.ModelViewSet):
     queryset = Blog.objects.all()
     serializer_class = BlogSerializer
-    permission_classes = [IsAuthenticated]
+    permission_classes = [ReadOnlyOrAuthenticated]
